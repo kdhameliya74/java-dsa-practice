a64b5eec61082aa63ebefc3a3de812fc735079c1
kdhameliya74 <kdhameliya94@gmail.com>
2026-01-16 17:39:44 +0530

    JDP: Reverse a string preserving space positions

--- Arrays/ReverseStringPreservingSpacePositions.java
+++ Arrays/ReverseStringPreservingSpacePositions.java
@@ -1,19 +1,23 @@
 package Arrays;
 
-// In Progress
-
 /*
  Problem: Reverse a string preserving space positions
  Difficulty: Easy
 
  Approach:
- 
+ - Convert the string to a character array.
+ - Use two pointers (left & right).
+ - Skip spaces on both ends.
+ - Swap only non-space characters.
+ - Spaces remain at their original indices. 
+
 
  Time Complexity: O(n)
- Space Complexity: O(1)
+ Space Complexity: O(n) due to chars array
 */
 
 public class ReverseStringPreservingSpacePositions {
+
     public static String nonOptimalSolution(String str) {
         int right = str.length() - 1;
         String newStr = "";
@@ -31,24 +35,43 @@ public class ReverseStringPreservingSpacePositions {
                 right--;
             }
         }
-
-        System.out.println(newStr);
-        return "";
+        return newStr;
     }
 
-    public static boolean optimalSolution() {
-        return true;
+    public static String optimalSolution(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        char[] charsArr = str.toCharArray();
+
+        while (left < right) {
+            if(charsArr[left] == ' ') {
+                left++;
+            }
+            else if(charsArr[right] == ' ') {
+                right--;
+            } else {
+                char temp = charsArr[left];
+                charsArr[left] =  charsArr[right];
+                charsArr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(charsArr);
     }
 
     public static void main(String[] args) {
 
         String str = "abc de";
+        String str1 = "internship at geeks for geeks";
 
-        // System.out.println("[1. optimalSolution] -> " + optimalSolution(s));
+        System.out.println("[1. optimalSolution] -> " + optimalSolution(str));
         System.out.println("[2. nonOptimalSolution] -> " + nonOptimalSolution(str));
-        // System.out.println("------------------------------------");
-        // System.out.println("[1. optimalSolution] -> " + optimalSolution(s1));
-        // System.out.println("[2. nonOptimalSolution] -> " + nonOptimalSolution(s1));
+        System.out.println("------------------------------------");
+        System.out.println("[1. optimalSolution] -> " + optimalSolution(str1));
+        System.out.println("[2. nonOptimalSolution] -> " + nonOptimalSolution(str1));
 
     }
 }
