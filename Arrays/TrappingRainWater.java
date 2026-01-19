cc94b7c2bf6b48aa91d4d20dd219b0f3018a4b39
kdhameliya74 <kdhameliya94@gmail.com>
2026-01-19 22:19:38 +0530

    JDP: Trapping Rain Water

--- Arrays/TrappingRainWater.java
+++ Arrays/TrappingRainWater.java
@@ -6,28 +6,74 @@ package Arrays;
  Difficulty: Hard
 
  Approach:
+ - Water trapped at any index depends on the minimum of:
+    max height to its left and max height to its right.
+    water = min(leftMax, rightMax) - height[i]
+
+ - Instead of using extra arrays for leftMax and rightMax,
+     we use two pointers starting from both ends.
+
+ - Maintain:
+    leftMax  → maximum height encountered from the left
+    rightMax → maximum height encountered from the right
 
  Time Complexity: O(n)
  Space Complexity: O(1)
 */
 
 public class TrappingRainWater {
-    public static void main(String[] args) {
-        int[] nums = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-        int totalWater = 0;
+    public static int optimalSolution(int[] height) {
         int left = 0;
-        int right = 1;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int water = 0;
 
         while (left < right) {
-            if(nums[left] < nums[right]) { // 0 < 1
-                right++; // [2] = 0 
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
             }
-            if(nums[left] > nums[right]) { // 0 > 0
-                left++; // [1] = 1
-            }
-            System.out.println("lop");
+        }
+        return water;
+    }
+
+    public static int nonOptimalSolution(int[] height) {
+        int len = height.length;
+        int water = 0;
+        int[] left = new int[len];
+        int[] right = new int[len];
+
+        left[0] = height[0];
+        right[len - 1] = height[len - 1];
+
+        // fill max left to right
+        for(int i = 1; i < len; i++) { // i = 1, 0-index has no boundary
+            left[i] = Math.max(left[i-1], height[i]);
+        }
+        
+        for(int i = len-2; i >= 0; i--) { // last element has no right boundary
+            right[i] = Math.max(right[i+1], height[i]);
         }
 
-        System.out.println(totalWater);
-    }    
+        for(int i= 0; i < len; i++) {
+            int minHeight = Math.min(left[i], right[i]);
+            water += minHeight - height[i];
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+        int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+        System.out.println("[1. optimalSolution] -> " + optimalSolution(height));
+        System.out.println("[2. nonOptimalSolution] -> " + nonOptimalSolution(height));
+
+    }
 }
