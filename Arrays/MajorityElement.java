ee09364d05149e8159b4a6d2fe33b2e4f97a5776
kdhameliya74 <kdhameliya94@gmail.com>
2026-02-12 19:10:39 +0530

    JDP: add correct solution

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -8,32 +8,68 @@ import java.util.Map;
  Difficulty: Easy
 
  Approach:
+ - We use a frequency HashMap to count occurrences of each element.
+ - While counting, if any element's frequency exceeds n/2,
+ - we return that element as the majority element.
+
+ - Alternatively, we can use Boyer–Moore Voting Algorithm
+ - which solves the problem in O(n) time and O(1) space.
 
  Time Complexity: O(n)
- Space Complexity: O(1)
+ Space Complexity: O(n)
 */
 
 public class MajorityElement {
-    public static void main(String[] args) {
-        int[] nums = {2,2,1,1,1,2,2};
+
+    public static int nonOptimalSolution(int[] nums) {
+        int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
         int majorElement = 0;
-        int majorValue = 0;
-        for(int num: nums) {
-            if(map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
-            }else {
-                map.put(num, 1);
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+            map.put(num, map.get(num) + 1);
+            } else {
+            map.put(num, 1);
             }
         }
 
-        System.out.println(map.toString());
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
-            if (entry.getValue() > majorValue) {
-                majorValue = entry.getValue();
-                majorElement = entry.getKey();
+        for (int num : map.keySet()) {
+            if (map.get(num) > n / 2) {
+                majorElement = num;
             }
         }
-        System.out.println(majorElement);
+        return majorElement;
+    }
+
+    public static int optimalSolution(int[] nums) {
+        // Boyer–Moore Code
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+        // int n = nums.length;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for (int num : nums) {
+        // map.put(num, map.getOrDefault(num, 0) + 1);
+
+        // if (map.get(num) > n / 2) {
+        // return num;
+        // }
+        // }
+        // return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2,2,1,1,1,2,2 };
+        System.out.println("[1. optimalSolution] -> " + optimalSolution(nums));
+        System.out.println("[2. nonOptimalSolution] -> " + nonOptimalSolution(nums));
     }
 }
