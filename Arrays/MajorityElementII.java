a8e23a0104d6d22ca7c7b10d739e8998a2ecb015
kdhameliya74 <kdhameliya94@gmail.com>
2026-02-13 21:48:32 +0530

    JDP-IP: bad solution

--- Arrays/MajorityElementII.java
+++ Arrays/MajorityElementII.java
@@ -1,9 +1,11 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
 
 /*
  Problem: Majority Element II
@@ -39,17 +41,28 @@ public class MajorityElementII {
     }
 
     public static String optimalSolution(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> elements = new ArrayList<>();
+        int candidate1 = 0;
+        int candidate2 = 0;
+        int count1 = 0;
+        int count2 = 0;
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-            if (map.get(num) > n / 3) {
-                elements.add(num);
+            if(candidate1 == num) {
+                count1++;
+            }
+            else if(candidate2 == num) {
+                count2++;
+            } else if(count1 ==0) {
+                candidate1 = num;
+            }
+            else if(count2 ==0) {
+                candidate2 = num;
+            } else {
+                count1--;
+                count2--;
             }
         }
-        return elements.toString();
+        int[] result = new int[]{candidate1, candidate2};
+            return Arrays.toString(result);
     }
 
     public static void main(String[] args) {
